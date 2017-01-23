fed752c57f2f05a7ed624b7a43e02bcd883feb58
Benji Altman <benji@altmansoftwaredesign.com>
2017-01-23 15:00:13 -0600

    split into two

--- js/interactive-svg.js
+++ js/interactive-svg.js
@@ -1,12 +1,24 @@
-$(function(){
+$(function () {
 
-	$("#stage").load('interactive.svg',function(response){
+	$("#britainStage").load('interactive.svg', function (response) {
 
 		$(this).addClass("svgLoaded");
-		
-		if(!response){ // Error loading SVG
+
+		if (!response) { // Error loading SVG
 			$(this).html('Error loading SVG. Be sure you are running from a the http protocol (not locally) and have read <strong><a href="http://tympanus.net/codrops/?p=13831#the-javascript">this important part of the tutorial</a></strong>');
 		}
 
 	});
+
+
+	$("#yorkStage").load('interactive.svg', function (response) {
+
+		$(this).addClass("svgLoaded");
+
+		if (!response) { // Error loading SVG
+			$(this).html('Error loading SVG. Be sure you are running from a the http protocol (not locally) and have read <strong><a href="http://tympanus.net/codrops/?p=13831#the-javascript">this important part of the tutorial</a></strong>');
+		}
+
+	});
+
 });
