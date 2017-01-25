70ffef282aa25cf1658cbb011942c650d923aa54
Astrorious <Astrorious@gmail.com>
2017-01-25 11:10:57 -0600

    jupkjo

--- js/interactive-svg.js
+++ js/interactive-svg.js
@@ -1,4 +1,4 @@
-function respond (response) {
+function respond(response) {
 		var t = $(this);
 		t.addClass("svgLoaded");
 		//t.attr("xmlns", "http://www.w3.org/2000/svg");
@@ -11,7 +11,7 @@ function respond (response) {
 		}
 
 
-
+}
 $(function () {
 
 	$("#britainStage").load('britishLine.svg', respond);
